a45b520c4590a67f5405ba354519e216f9e63e07
Jose Ortega <s3779997@student.rmit.edu.au>
2025-06-10 23:19:35 +1000

    added footer elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 export default function Footer() {
     return (
         <footer>
-            <h1>Footer</h1>
+            <h1 className="footer-h1">Footer</h1>
+            <div className="footer-links">
+                <div><a href="https://www.rmit.edu.au/">RMIT Website</a></div>
+                <div><a href="https://www.rmit.edu.au/students/my-course/canvas">Canvas</a></div>
+                <div><a href="https://www.rmit.edu.au/students/support-services/student-connect">Student Connect</a></div>
+                <div><a href="https://www.rmit.edu.au/students/my-course/program-course-information/course-guides">Course Guide</a></div>
+            </div>
+            <p id="footer-copyright">Copyright © 2025 RMIT University</p>
         </footer>
     )
 }
